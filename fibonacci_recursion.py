302395c7360a2514ed21291933e5b42dfa5c53c1
Dhruv <djain@clemson.edu>
2021-03-27 16:50:11 -0400

    final

--- fibonacci_recursion.py
+++ fibonacci_recursion.py
@@ -1,6 +1,10 @@
 # created by Dhruv Jain on March 27th 2021
 
 def fibonacci(n):
-    return fibonacci(n-1) + fibonacci(n-2)
+    assert n>=0 and int(n) == n, "Fibonacci cannot be negative or non integer"
+    if n in [0,1]:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
-fibonacci(5)
+print(fibonacci(-7))
