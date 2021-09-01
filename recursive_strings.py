cfcd1e64b0237fab166262c1b8a867542ca7fd6d
Dhruv Jain <djain@clemson.edu>
2021-09-01 15:00:04 -0400

    Update recursive_strings.py

--- recursive_strings.py
+++ recursive_strings.py
@@ -1,4 +1,14 @@
+#Print reverse of a string 
 def reverse(strng):
     if len(strng) == 1:
         return strng[0]
     return strng[-1] + reverse(strng[:-1])
+
+#Return true if string is palindrome (same from start or end example - tacocat)
+def isPalindrome(strng):
+    if len(strng) == 1:
+        return True
+    if strng[0] == strng[-1]:
+        return isPalindrome(strng[1:-1])
+    return False
+    
